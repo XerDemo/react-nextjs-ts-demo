0205f4b65b6b1010ec8f6b946207a1ba67f8afdf
XerDemo <1492113422@qq.com>
2023-01-08 17:31:13 +0800

    feat: 引入antd并且自定义主题和字体大小，引入emotion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,26 @@
+import { Button, ConfigProvider } from "antd";
 import Layout from "components/layout";
 import type { AppProps } from "next/app";
+
 import "normalize.css/normalize.css";
 import "../styles/globals.css";
 
 // _app.tsx 入口文件
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    // <ConfigProvider></ConfigProvider> 是antd用来自定义配置的
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#8cf0c6ec",
+          fontSize: 16,
+        },
+      }}
+    >
+      <Layout>
+        <Component {...pageProps} />
+        <Button>anniu</Button>
+      </Layout>
+    </ConfigProvider>
   );
 }
