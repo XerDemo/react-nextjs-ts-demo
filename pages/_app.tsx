513294c8aeed34e7690c39d8e4f5a06774ce30ac
XerDemo <1492113422@qq.com>
2023-01-08 15:16:25 +0800

    feat: 干净的工程目录

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
+// _app.tsx 入口文件
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
