54a159af624245537e4ac1f673ef223feb790e4a
XerDemo <1492113422@qq.com>
2023-01-08 17:01:44 +0800

    feat: normal.css+ 1rem=10px

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import Layout from "components/layout";
 import type { AppProps } from "next/app";
+import "normalize.css/normalize.css";
+import "../styles/globals.css";
 
 // _app.tsx 入口文件
 export default function App({ Component, pageProps }: AppProps) {
